1a6bafe8c4ea333ba8bbdc5088ae2de979896478
Syafiq Shamsuddin <Shyuffix555@gmail.com>
2019-07-09 18:40:26 +0800

    okay ni dah okay

--- js/login.js
+++ js/login.js
@@ -49,7 +49,7 @@ async function login() {
       },
       body: formData
     });
-
+    
     const success = await loginResponse.json();
     console.log(success);
     if (success.status !== -1) {
