a40afe70726f9af558995d520f30859a208afbcd
Syafiq Shamsuddin <Shyuffix555@gmail.com>
2019-07-15 14:57:10 +0800

    use helper for outputting region

--- js/index.js
+++ js/index.js
@@ -13,6 +13,14 @@ $(document).ready(function () {
     return moment(timestamp).format('DD MMMM YYYY')
   });
 
+  Handlebars.registerHelper("printRegion", function ({ data }) {
+    const { oper, regions } = data.root
+    const { index } = data
+    const operator = oper[index]
+    const region = regions.find(region => region.id === operator.region_id)
+    return region ? region.name : 'NULL'
+  });
+
   Handlebars.registerHelper("json", function (content) {
     return JSON.stringify(content);
   });
@@ -143,7 +151,7 @@ $(document).ready(function () {
           Authorization: `bearer ${sessionStorage.getItem("token")}`
         }
       });
-      
+
       const oper = await res.json();
       console.log(oper);
 
