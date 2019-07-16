ee0c1908ad05d32df48ab0386acf2c6534550be6
MUHAMMAD IRSYAD BIN KAMIL RIADZ <irsyadkamil96@gmail.com>
2019-07-16 15:36:58 +0800

    change url

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 // const url = "https://ccs.cyrix.my/CCS-API/";
-const url = "http://localhost/CCS-API/";
+// const url = "http://localhost/CCS-API/";
+const url = "https://cyrixmy-api.herokuapp.com/";
 async function login() {
   event.preventDefault(); //prevent redirect/page refresh
   var formData = JSON.stringify({
